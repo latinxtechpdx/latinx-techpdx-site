82335042ef98258cee42835d823b9c7ed0fbcd84
fvc <lagosoundsoftware@gmail.com>
2018-02-26 23:04:31 -0800

    Add and setup font files, PNG images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/latinxtechpdx_map.png';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="App-title">
+            {/* Testing font weight setup */}
+            <span className="Thin">Welcome </span>
+            <span className="Light">to Latinx </span>
+            <span className="Normal">Tech </span>
+            <span className="Black">PDX</span>
+          </div>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
